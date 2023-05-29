715add1a3ee8c9cf9f2ef0aa8a5add09277236ee
Toxous <112708093+Toxous@users.noreply.github.com>
2023-05-29 09:40:48 -0300

    Add files via upload
    Projeto Desenvolvimento Web II

---
+++ forms.js
@@ -0,0 +1,9 @@
+function funcao(){
+    var nome = document.getElementById("nome").value;
+    var matr = document.getElementById("matr").value;
+    var email = document.getElementById("email").value;
+    var area = document.getElementById("area").value;
+    var cH = document.getElementById("carga").value;
+
+    console.log(nome);
+}
